f8d01605660191c222d3aa961ed9ab95ffdd4895
kekeke001 <52167708+kekeke001@users.noreply.github.com>
2020-02-10 23:41:54 +0800

    Add files via upload

--- merge_health_report.py
+++ merge_health_report.py
@@ -1,9 +1,10 @@
 import extract_employee
-import extract_health_report
+#import extract_health_report
 
 
 if __name__ == '__main__':
     # 读取员工数据
     _emp = extract_employee.get_employee_data()
+    print(_emp)
     # 读取打卡数据
-    _health_report = extract_health_report.get_health_report_data()
+    #_health_report = extract_health_report.get_health_report_data()
