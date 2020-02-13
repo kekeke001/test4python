a23f26eda4a329bcb1d9791e94c91b45c021f887
qianmin <coffee.qm@gmail.com>
2020-02-14 00:12:41 +0800

    update xlsx utils, add function:merge_files

--- merge_health_report.py
+++ merge_health_report.py
@@ -9,6 +9,11 @@ if __name__ == '__main__':
     # 读取打卡数据
     # _health_report = extract_health_report.get_health_report_data()
     # print(_health_report)
-    _data = xlsx_utils.merge(source_path=[r'.\test\resources\employee.xlsx ', r'.\test\resources\health_report.xlsx'],
-                             sheet_name='Sheet1')
-    xlsx_utils.write(file_path=r'.\test\results\merge.xlsx', sheet_name='Sheet1', data=_data)
+    # _data = xlsx_utils.merge(source_path=[r'.\test\resources\employee.xlsx ', r'.\test\resources\health_report.xlsx'], sheet_name='Sheet1')
+    # xlsx_utils.write(file_path=r'.\test\results\merge.xlsx', sheet_name='Sheet1', data=_data)
+    #
+    _files = []
+    _files.append({"path": r".\test\resources\employee.xlsx", "sheet": "Sheet1", "key_column": "WorkSN"})
+    _files.append({"path": r".\test\resources\health_report.xlsx", "sheet": "Sheet1", "key_column": "工号"})
+    _data = xlsx_utils.merge_files(_files)
+    xlsx_utils.write(file_path=r'.\test\results\merge_files.xls', sheet_name='Sheet1', data=_data)
