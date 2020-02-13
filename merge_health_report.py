ce93771733a6f0fd6c771933f3f694f4a89c52d2
kekeke001 <52167708+kekeke001@users.noreply.github.com>
2020-02-13 20:27:01 +0800

    Update merge_health_report.py

--- merge_health_report.py
+++ merge_health_report.py
@@ -1,10 +1,14 @@
 import extract_employee
-#import extract_health_report
-
+import extract_health_report
+import xlsx_utils
 
 if __name__ == '__main__':
     # 读取员工数据
-    _emp = extract_employee.get_employee_data()
-    print(_emp)
+    # _emp = extract_employee.get_employee_data()
+    # print(_emp)
     # 读取打卡数据
-    #_health_report = extract_health_report.get_health_report_data()
+    # _health_report = extract_health_report.get_health_report_data()
+    # print(_health_report)
+    _data = xlsx_utils.merge(source_path=[r'.\test\resources\employee.xlsx ', r'.\test\resources\health_report.xlsx'],
+                             sheet_name='Sheet1')
+    xlsx_utils.write(file_path=r'.\test\results\merge.xlsx', sheet_name='Sheet1', data=_data)
